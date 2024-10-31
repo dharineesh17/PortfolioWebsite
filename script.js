73c07e7218279baaa63a69dd94c348533fb17410
Dharineesh K S <88188364+dharineesh17@users.noreply.github.com>
2024-10-31 20:35:41 +0530

    Release Version 2.1: fix error in comments

--- script.js
+++ script.js
@@ -64,17 +64,17 @@ sections.forEach(section => {
 // Disable right-click context menu
 document.addEventListener('contextmenu', function(event) {
     event.preventDefault();
-// });
+});
 
 // Disable text selection
 document.addEventListener('selectstart', function(event) {
     event.preventDefault();
-// });
+});
 
 // Disable Ctrl+C (Copy)
 document.addEventListener('copy', function(event) {
     event.preventDefault();
-// });
+});
 
 // Disable Ctrl+V (Paste)
 document.addEventListener('paste', function(event) {
